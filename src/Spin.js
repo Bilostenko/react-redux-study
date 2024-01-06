4eee56794a4c18908eb0f5813d7a2abf0319109f
Bilostenko <bilostenkod@gmail.com>
2024-01-06 13:54:59 +0200

    add loader

---
+++ src/Spin.js
@@ -0,0 +1,11 @@
+import Loader from 'react-loader-spinner';
+
+const Spin = (props) => {
+  return (
+    <div className="loader-styles">
+    <Loader
+    
+    />
+    </div>
+  )
+}
--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import {likesReducer} from "./likesReducer";
 import { inputReducer } from "./inputReducer";
 import { commentReducer } from "./commentReducer";
+import { appReducer } from "./appReducer";
 
 export const rootReducer = combineReducers({
   like: likesReducer,
   text: inputReducer,
-  commentReducer
+  commentReducer,
+  appReducer
 })
