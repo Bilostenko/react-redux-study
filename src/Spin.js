b8dec2e2984305828fbaedba7819385d9b13572e
Bilostenko <bilostenkod@gmail.com>
2024-01-07 00:30:05 +0200

    add spin

--- src/Spin.js
+++ src/Spin.js
@@ -1,11 +1,21 @@
 import Loader from 'react-loader-spinner';
+import { useSelector } from 'react-redux';
 
 const Spin = (props) => {
+
+const spinner = useSelector(state => state.appReducer.loading)
+
   return (
     <div className="loader-styles">
     <Loader
-    
+    type="TailSPin"
+    color="#00BFFF"
+    height={100}
+    width={100}
+    visible={false}
     />
     </div>
   )
 }
+
+export default Spin
